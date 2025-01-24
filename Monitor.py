1af10f5b3fa5d403c29967b40ea084e9e034fef2
hazeyblu <48317480+hazeyblu@users.noreply.github.com>
2025-01-25 00:05:04 +0530

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -81,15 +81,13 @@ st.markdown(
 # Add a background color to the Intraweek row and set font color to white
 df_styled = df.style.apply(
     lambda x: ["background-color: #585858; color: white;" if x.name == 0 else "" for _ in x], axis=1
-).set_properties(**{'text-align': 'center'})  # Center-align text
+)
 
-# Display the table without index numbers and centered
-st.write(
+# Display the table centered on the page
+st.markdown(
     f"""
-    <style>
-        .stTable {{ margin: 0 auto; }}
-    </style>
-    {df_styled.to_html(index=False, escape=False)}
+    <div style="display: flex; justify-content: center;">
+        {df_styled.to_html(index=False, escape=False, justify='center')}
     """,
     unsafe_allow_html=True
 )
